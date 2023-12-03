9e3f881ff6bdf651ee1031cba41738e5f5c171fa
KaueMartins1924 <kaue.silvamartinsshopee@gmail.com>
2023-12-03 15:16:24 -0300

    Versão 2

--- versao_1.js
+++ versao_1.js
@@ -10,28 +10,28 @@
  * 
  * 
  */
-console.log ("Bem vindo ao jogo!")
-let iniciar = confirm("Deseja iniciar o jogo?")
-if (iniciar){
-   let carta1 = comprarCarta()
-   let carta2 = comprarCarta()
-   let cartaPc1 = comprarCarta()
-   let cartaPc2 = comprarCarta()
+// console.log ("Bem vindo ao jogo!")
+// let iniciar = confirm("Deseja iniciar o jogo?")
+// if (iniciar){
+//    let carta1 = comprarCarta()
+//    let carta2 = comprarCarta()
+//    let cartaPc1 = comprarCarta()
+//    let cartaPc2 = comprarCarta()
 
-   let soma = carta1.valor+carta2.valor
-   let somaPc = cartaPc1.valor+cartaPc2.valor
-   const perder = 21
-console.log(`Usuario - cartas: ${carta1.texto}, ${carta2.texto} = ${soma}. \nComputador - cartas: ${cartaPc1.texto}, ${cartaPc2.texto} = ${somaPc}.`)
-if (soma > somaPc && soma <=perder){
-   console.log("Você ganhou!")
-}else if(soma > somaPc && soma >perder){
-   console.log("O computador ganhou!")
-}else if(soma < somaPc && somaPc <=perder){
-   console.log("O computador ganhou!")
-}else if (soma < somaPc && somaPc >perder){
-   console.log ("Você ganhou!")
-}else{
-   console.log ("O jogo empatou!")
-}
-}
+//    let soma = carta1.valor+carta2.valor
+//    let somaPc = cartaPc1.valor+cartaPc2.valor
+//    const perder = 21
+// console.log(`Usuario - cartas: ${carta1.texto}, ${carta2.texto} = ${soma}. \nComputador - cartas: ${cartaPc1.texto}, ${cartaPc2.texto} = ${somaPc}.`)
+// if (soma > somaPc && soma <=perder){
+//    console.log("Você ganhou!")
+// }else if(soma > somaPc && soma >perder){
+//    console.log("O computador ganhou!")
+// }else if(soma < somaPc && somaPc <=perder){
+//    console.log("O computador ganhou!")
+// }else if (soma < somaPc && somaPc >perder){
+//    console.log ("Você ganhou!")
+// }else{
+//    console.log ("O jogo empatou!")
+// }
+// }
 
--- versao_2.js
+++ versao_2.js
@@ -1,13 +1,68 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+   /**
+    * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+    * 
+    * 
+       const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+      
+      console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+      console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+   * 
+   * 
+   * 
+   */
 
+
+//definir jogadores
+let jogador = []
+let computador = []
+
+function comprarCartaAoJogador() {
+   jogador.push(comprarCarta())
+}
+function comprarCartaAoComputador() {
+   computador.push(comprarCarta())
+}
+function calcularPontuacao(cartas) {
+   return cartas.reduce((total, carta) => total + carta.valor, 0)
+}
+
+function mostrarCartas(participante1, cartas1, participante2, cartas2) {
+   let textoCartas1 = cartas1.map(carta => carta.texto).join(" ")
+   let textoCartas2 = cartas2.map(carta => carta.texto).join(" ")
+   let pontuacaoJogador = calcularPontuacao(cartas1)
+   let pontuacaoComputador = calcularPontuacao(cartas2)
+
+   let comprarParaJogador = confirm(`${participante1}: ${textoCartas1}\n${participante2}: ${textoCartas2.slice(-3)}\nDeseja comprar mais uma carta?`)
+   
+   if (comprarParaJogador && pontuacaoJogador < 21) {
+      comprarCartaAoJogador()
+   }
+   if (pontuacaoJogador >= 21) {
+      alert(`O computador venceu! \n\nSuas cartas: ${textoCartas1}\nSua pontuação: ${pontuacaoJogador}\n\nCartas do computador: ${textoCartas2}\nPontuação do computador: ${pontuacaoComputador}`)
+   } else if (pontuacaoComputador >= 21) {
+      alert(`Você venceu! \n\nSuas cartas: ${textoCartas1}\nSua pontuação: ${pontuacaoJogador}\n\nCartas do computador: ${textoCartas2}\nPontuação do computador: ${pontuacaoComputador}`)
+   } else if (!comprarParaJogador) {
+      while (pontuacaoComputador <= 21) {
+         comprarCartaAoComputador()
+         pontuacaoComputador = calcularPontuacao(computador)
+      }
+      if (pontuacaoComputador > 21) {
+         alert(`Você venceu! \n\nSuas cartas: ${textoCartas1}\nSua pontuação: ${pontuacaoJogador}\n\nCartas do computador: ${textoCartas2}\nPontuação do computador: ${pontuacaoComputador}`)
+      } else {
+         alert("Fim de jogo!")
+      }
+   } else {
+      mostrarCartas("Suas cartas", jogador, "Carta revelado do computador", computador)
+   }
+}
+function inicio() {
+   let PerguntaInicio = confirm(`Bem-vindo ao jogo de Blackjack! \nQuer iniciar uma nova rodada?`)
+   if (PerguntaInicio) {
+      comprarCartaAoJogador()
+      comprarCartaAoJogador()
+      comprarCartaAoComputador()
+      comprarCartaAoComputador()
+      mostrarCartas("Suas cartas", jogador, "Carta revelado do computador", computador)
+   }
+}
+inicio()
